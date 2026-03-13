5e9f1b463ad1e68c693f14e5df4d0cbf21da6a03
TranMaiPhucThinh <thinhtran0823@gmail.com>
2026-03-13 12:53:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
+
+const ApiError = require("./app/api-error");
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
